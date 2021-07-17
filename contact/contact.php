4082b4a9eb01243af49947ece6c755882eb0d095
Prachi Mehra <68285981+Prachimehra15@users.noreply.github.com>
2021-07-17 16:09:04 +0530

    Update contact.php

--- contact/contact.php
+++ contact/contact.php
@@ -1,1 +1,20 @@
+<?php
+$servername ="localhost";
+$username ="root";
+$password ="";
+$database ="contact";
+  $con = mysqli_connect('localhost','root','','contact');
 
+  $name=$_POST['name'];
+  $email=$_POST['email'];
+  $comments=$_POST['comments'];
+  
+
+  $sql="INSERT  INTO `info`(`name`,`email`,`comments`)VALUES
+  ('$name','$email','$comments')";
+  $rs=mysqli_query($con,$sql);
+  if($rs)
+  {
+      echo "inserted";
+     }
+ ?>
