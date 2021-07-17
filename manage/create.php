58d5da768d12930376db05338f5a7e30661732e4
Prachi Mehra <68285981+Prachimehra15@users.noreply.github.com>
2021-07-17 16:11:23 +0530

    Add files via upload

---
+++ manage/create.php
@@ -0,0 +1,117 @@
+<?php
+// Include config file
+require_once "config.php";
+ 
+// Define variables and initialize with empty values
+$username = $email = $level = "";
+$name_err = $email_err = $level_err = "";
+ 
+// Processing form data when form is submitted
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    // Validate name
+    $input_name = trim($_POST["username"]);
+    if(empty($input_name)){
+        $name_err = "Please enter a name.";
+    } elseif(!filter_var($input_name, FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>"/^[a-zA-Z\s]+$/")))){
+        $name_err = "Please enter a valid name.";
+    } else{
+        $username = $input_name;
+    }
+    
+    // Validate email
+    $input_email = trim($_POST["email"]);
+    if(empty($input_email)){
+        $email_err = "Please enter an email.";     
+    } else{
+        $email = $input_email;
+    }
+    
+    // Validate level
+    $input_level = trim($_POST["level"]);
+    if(empty($input_level)){
+        $level_err = "Please enter the level amount.";     
+    } 
+     else{
+        $level = $input_level;
+    }
+    
+    // Check input errors before inserting in database
+    if(empty($name_err) && empty($email_err) && empty($level_err)){
+        // Prepare an insert statement
+        $sql = "INSERT INTO users (username, email, level) VALUES (?, ?, ?)";
+         
+        if($stmt = mysqli_prepare($link, $sql)){
+            // Bind variables to the prepared statement as parameters
+            mysqli_stmt_bind_param($stmt, "sss", $param_name, $param_email, $param_level);
+            
+            // Set parameters
+            $param_name = $username;
+            $param_email = $email;
+            $param_level = $level;
+            
+            // Attempt to execute the prepared statement
+            if(mysqli_stmt_execute($stmt)){
+                // Records created successfully. Redirect to landing page
+                header("location: index.php");
+                exit();
+            } else{
+                echo "Something went wrong. Please try again later.";
+            }
+        }
+         
+        // Close statement
+        mysqli_stmt_close($stmt);
+    }
+    
+    // Close connection
+    mysqli_close($link);
+}
+?>
+ 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Create Record</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <style type="text/css">
+        .wrapper{
+            width: 500px;
+            margin: 0 auto;
+        }
+    </style>
+</head>
+<body>
+    <div class="wrapper">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="page-header">
+                        <h2>Create Record</h2>
+                    </div>
+                    <p>Please fill this form and submit to add user record to the database.</p>
+                    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+                        <div class="form-group <?php echo (!empty($name_err)) ? 'has-error' : ''; ?>">
+                            <label>Name</label>
+                            <input type="text" name="name" class="form-control" value="<?php echo $username; ?>">
+                            <span class="help-block"><?php echo $name_err;?></span>
+                        </div>
+                        <div class="form-group <?php echo (!empty($email_err)) ? 'has-error' : ''; ?>">
+                            <label>email</label>
+                            <textarea name="email" class="form-control"><?php echo $email; ?></textarea>
+                            <span class="help-block"><?php echo $email_err;?></span>
+                        </div>
+                        <div class="form-group <?php echo (!empty($level_err)) ? 'has-error' : ''; ?>">
+                            <label>level</label>
+                            <input type="text" name="level" class="form-control" value="<?php echo $level; ?>">
+                            <span class="help-block"><?php echo $level_err;?></span>
+                        </div>
+                        <input type="submit" class="btn btn-primary" value="Submit">
+                        <a href="index.php" class="btn btn-default">Cancel</a>
+                    </form>
+                </div>
+            </div>        
+        </div>
+    </div>
+</body>
+</html>
